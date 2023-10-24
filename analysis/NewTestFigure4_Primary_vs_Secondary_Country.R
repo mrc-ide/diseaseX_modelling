5fd81fba750ebf45845a0f1b8446f84e6b01e15b
CharlieWhittaker <cwhittaker1000@gmail.com>
2023-10-24 20:01:14 +0100

    new secondary country infrastructure in

--- analysis/NewTestFigure4_Primary_vs_Secondary_Country.R
+++ analysis/NewTestFigure4_Primary_vs_Secondary_Country.R
@@ -2,98 +2,9 @@
 source(here::here("main.R"))
 
 # Load required functions
+source(here::here("functions/helper_functions.R"))
 source(here::here("functions/run_sars_x.R"))
 source(here::here("functions/runs_sars_x_secondary_country.R"))
-
-# Plotting the dynamics in source, early and late secondary countries
-population <- squire:::get_population("Argentina")
-population <- 10^6 * population$n / sum(population$n)
-mixing_matrix <- squire:::get_mixing_matrix("Argentina")
-test_run <- run_booster(time_period = 1300,
-                        contact_matrix_set = mixing_matrix,
-                        population = population,
-                        R0 = 1.15,     
-                        tt_R0 = 0, 
-                        hosp_bed_capacity = 10^9,                                     
-                        ICU_bed_capacity = 10^9,                                       
-                        dur_R = 365000000000,                                                        
-                        seeding_cases = 10,
-                        dur_V = 365000000000,                                              
-                        primary_doses = rep(0, 1300),  
-                        second_doses = rep(0, 1300),
-                        booster_doses = rep(0, 1300))
-
-buffer <- 125
-infections <- nimue::format(test_run, compartments = "S", summaries = "infections") %>%
-  filter(compartment != "S") %>%
-  mutate(t = t + buffer) %>%
-  select(-replicate) %>%
-  mutate(value = ifelse(is.na(value), 0, value)) %>%
-  mutate(R0 = 1.15) %>%
-  complete(t = seq(0, max(t), 1),
-           fill = list(value = 0.0920248, R0 = 1.15, compartment = "infections"))
-
-secondary_early_start <- 200
-infections2 <- infections %>%
-  mutate(t = t + secondary_early_start) %>%
-  complete(t = seq(0, max(t), 1),
-           fill = list(value = NA, R0 = 1.15, compartment = "infections"))
-
-secondary_late_start <- 650
-infections3 <- infections %>%
-  mutate(t = t + secondary_late_start) %>%
-  complete(t = seq(0, max(t), 1),
-           fill = list(value = NA, R0 = 1.15, compartment = "infections"))
-
-base_plot <- ggplot(data = infections3, aes(x = t, y = value)) +
-  geom_hline(yintercept = 0, color = "black", linetype="solid") + # Adds vertical line at x=0
-  geom_line(linewidth = 1, col = "#70C243") +
-  geom_line(data = infections2, aes(x = t, y = value), linewidth = 1, col = "#e5a445") +
-  geom_line(data = infections, aes(x = t, y = value), linewidth = 1, col = "#ca4d3d") +
-  geom_point(x = 0, y = 0, pch = 21, fill = "#ca4d3d", col = "black", size = 2) +
-  geom_point(x = secondary_early_start, y = 0, pch = 21, fill = "#e5a445", col = "black", size = 2) +
-  geom_point(x = secondary_late_start, y = 0, pch = 21, fill = "#70C243", col = "black", size = 2) +
-  
-  theme_bw() +
-  coord_cartesian(xlim = c(0, 1500), ylim = c(-200, 1000)) +
-  
-  geom_segment(x = 0, xend = secondary_early_start, 
-               y = -100, yend = -100, 
-               linewidth = 1, col = "black", linetype = "solid") +
-  geom_point(x = 0, y = -100, pch = 21, fill = "#ca4d3d", col = "black", size = 2) +
-  geom_point(x = secondary_early_start, y = -100, pch = 21, fill = "#e5a445", col = "black", size = 2) +
-  annotate("text", x = secondary_early_start + 25, y = -100, label = "Earlier Importation", color = "black", hjust = 0) +
-  
-  geom_segment(x = 0, xend = secondary_late_start, 
-               y = -200, yend = -200, 
-               linewidth = 1, col = "black", linetype = "solid") +
-  geom_point(x = 0, y = -200, pch = 21, fill = "#ca4d3d", col = "black", size = 2) +
-  geom_point(x = secondary_late_start, y = -200, pch = 21, fill = "#70C243", col = "black", size = 2) +
-  annotate("text", x = secondary_late_start + 25, y = -200, label = "Later Importation", color = "black", hjust = 0) +
-  
-  labs(x = "", y = "Daily Incidence") +
-  # theme_cowplot() +
-  theme(axis.text = element_blank(),
-        axis.ticks = element_blank(),
-        axis.line.x = element_blank()) +
-  geom_rect(xmin = 1300, xmax = 1325, ymin = 950, ymax = 1000, fill = "#ca4d3d", col = "#ca4d3d") +
-  geom_rect(xmin = 1300, xmax = 1325, ymin = 875, ymax = 925, fill = "#e5a445", col = "#e5a445") +
-  geom_rect(xmin = 1300, xmax = 1325, ymin = 800, ymax = 850, fill = "#70C243", col = "#70C243") +
-  
-  annotate("text", x = 1340, y = 975, label = "Source Country", color = "black", hjust = 0) +
-  annotate("text", x = 1340, y = 900, label = "Early Importer", color = "black", hjust = 0) +
-  annotate("text", x = 1340, y = 825, label = "Late Importer", color = "black", hjust = 0)
-
-
-image_path <- magick::image_read_pdf(path = "test_primarySecondary_figure.pdf")
-base_ft_inset <- ggdraw() +
-  draw_image(image_path, 
-             x = 0.0, y = 0.85, hjust = 0, vjust = 1, scale = 2, width = 0.25, height = 0.25) +
-  draw_plot(base_plot) +
-  theme_bw()
-
-# 10.91 * 3.45 --> 11 * 3.5
-
 
 ### OPTION 2: ALL IN TERMS OF CALENDAR DAYS, DIFFERENT NPIs CONSIDERED
 
@@ -114,16 +25,18 @@ raw_rel_start_scenarios <- create_scenarios(R0 = c(1.5, 2.5, 3.5),              
                                             dur_R = 365000000,                             # duration of infection-induced immunity
                                             dur_bpsv = 365000000,                          # duration of BPSV vaccine immunity
                                             dur_spec = 365000000,                          # duration of disease-specific vaccine immunity
-                                            coverage = 0.8,                                # proportion of the population vaccinated
-                                            vaccination_rate = 0.035,                      # vaccination rate per week as percentage of population
+                                            coverage_bpsv = 0.8,                           # proportion of the population vaccinated
+                                            coverage_spec = 0.8,                           # proportion of the population vaccinated
+                                            vaccination_rate_bpsv = 0.035, 
+                                            vaccination_rate_spec = 0.035,
                                             seeding_cases = 5,                             # number of initial seeding cases
                                             min_age_group_index_priority = 13,             # index of the youngest age group given priority w.r.t vaccines (13 = 60+)
                                             min_age_group_index_non_priority = 4,          # index of the youngest age group that *receives* vaccines (4 = 15+)
                                             runtime = 1250)
 
-sequence <-  seq(100, -100, -10)
+sequence <-  seq(100, -100, -5)
 raw_rel_start_scenarios2 <- expand_grid(raw_rel_start_scenarios, days_source_detection_is_ahead_arrival_secondary = sequence[-which(sequence == 0)])
-raw_rel_start_scenarios2$detection_time_secondary <- 10 # need to change this to make it R0 specific - also be wary when this is longer than time_to_bpsv_coverage - need to change NPIs_arrival_after subset in the NPIs generator
+raw_rel_start_scenarios2$detection_time_secondary <- 7 # Are we okay for this to be in calendar days and independent of R0? (Potentially yes if we think enhanced surveillance is the play?)
 
 ## Generating NPIs based on specific detection times, R0, and other vaccine-related events
 NPIs_raw_rel_start <- default_NPI_scenarios_secondary(lockdown_Rt = 0.9, minimal_mandate_reduction = 0.25, 
@@ -131,7 +44,8 @@ NPIs_raw_rel_start <- default_NPI_scenarios_secondary(lockdown_Rt = 0.9, minimal
                                                       scenarios = raw_rel_start_scenarios2)
 rel_start_scenarios <- raw_rel_start_scenarios2 %>%
   full_join(NPIs_raw_rel_start, by = c("R0", "country", "population_size", "bpsv_start", "detection_time_secondary",   # joining by all columns which influence NPI scenario timing
-                                       "days_source_detection_is_ahead_arrival_secondary", "specific_vaccine_start", "vaccination_rate", "coverage", "min_age_group_index_priority"), multiple = "all") %>%
+                                       "days_source_detection_is_ahead_arrival_secondary", "specific_vaccine_start", "vaccination_rate_bpsv", "vaccination_rate_spec", 
+                                       "coverage_bpsv", "coverage_spec", "min_age_group_index_priority"), multiple = "all") %>%
   select(-detection_time.x) %>%
   rename(detection_time = detection_time.y)
 
@@ -143,8 +57,8 @@ rel_start_scenarios2 <- rel_start_scenarios %>%
   mutate(scenario_index = 1:n())
 
 ## Running the model and summarising the output
-cores <- parallel::detectCores() - 2
-fresh_run <- FALSE
+cores <- parallel::detectCores() - 3
+fresh_run <- TRUE
 if (fresh_run) {
   plan(multisession, workers = cores) # multicore does nothing on windows as multicore isn't supported
   system.time({out <- future_pmap(rel_start_scenarios2, run_sars_x, .progress = TRUE, .options = furrr_options(seed = 123))})
@@ -212,24 +126,6 @@ annotation_data3 <- data.frame(
 deaths_averted <- ggplot(model_outputs2) +
   annotate("rect", xmin = -105, xmax = 0, ymin = -Inf, ymax = Inf, fill = "grey", alpha = 0.25) +
   annotate("rect", xmin = 0, xmax = 100, ymin = -Inf, ymax = Inf, fill = "white", alpha = 0.1) +
-  geom_rect(data = annotation_data1, 
-            aes(xmin = xmin, xmax = xmax, ymin = ymin, ymax = ymax), 
-            inherit.aes = FALSE, 
-            fill = c("#70C243", "#70C243", "#70C243", "#70C243"), 
-            colour = c("#70C243", "#70C243", "#70C243", "#70C243"), 
-            alpha = 0.2) +
-  geom_rect(data = annotation_data2, 
-            aes(xmin = xmin, xmax = xmax, ymin = ymin, ymax = ymax), 
-            inherit.aes = FALSE, 
-            fill = c("#e5a445", "#e5a445", "#e5a445", "#e5a445"), 
-            colour = c("#e5a445", "#e5a445", "#e5a445", "#e5a445"), 
-            alpha = 0.2) +
-  geom_rect(data = annotation_data3, 
-            aes(xmin = xmin, xmax = xmax, ymin = ymin, ymax = ymax), 
-            inherit.aes = FALSE, 
-            fill = c("#ca4d3d", "#ca4d3d", "#ca4d3d", "#ca4d3d"), 
-            colour = c("#ca4d3d", "#ca4d3d", "#ca4d3d", "#ca4d3d"), 
-            alpha = 0.2) +
   geom_line(aes(x = -days_source_detection_is_ahead_arrival_secondary, 
                 y = bpsv_deaths_averted * 1000 / population_size, col = factor(R0))) +
   geom_point(aes(x = -days_source_detection_is_ahead_arrival_secondary, 
@@ -250,7 +146,7 @@ deaths_averted <- ggplot(model_outputs2) +
        y = "Deaths Averted By BPSV (Per 1,000 Population)") +
   geom_vline(xintercept = 0, linewidth = 0.25, linetype = "dashed") +
   theme(strip.placement = "outside",
-        legend.position = "none",
+        legend.position = "right",
         strip.background = element_rect(fill="white")) +
   coord_cartesian(xlim = c(-100, 100))
 
@@ -395,100 +291,3 @@ cowplot::plot_grid(base_plot, bottom_half,
                    nrow = 2, 
                    rel_heights = c(1, 3), labels = c("A", ""))
 # 9 x 8
-
-
-
-
-
-###########################################
-
-# cowplot::plot_grid(base_ft_inset, temp_plot, axis = "hv", align = "tblr", nrow = 2, rel_heights = c(1, 2), labels = c("A", "B"))
-
-
-# # OPTION 1: ALL IN TERMS OF CALENDAR DAYS, NO NPIs JUST UNMITIGATED
-# 
-# #### Generate initial sets of scenarios (note placeholder for detection time)
-# raw_rel_start_scenarios <- create_scenarios(R0 = c(1.5, 2, 2.5, 3, 3.5),                   # Basic reproduction number
-#                                             IFR = 1,                                       # IFR
-#                                             population_size = 10^10,
-#                                             Tg = 6.7,                                      # Tg
-#                                             detection_time = 1,                            # PLACEHOLDER
-#                                             bpsv_start = 0,                                # BPSV distribution start (time after detection time)
-#                                             bpsv_protection_delay = 7,                     # delay between receipt of BPSV dose and protection
-#                                             specific_vaccine_start = c(100, 200, 365),     # specific vaccine distribution start (time after detection time)
-#                                             specific_protection_delay = 7,                 # delay between receipt of specific dose and protection
-#                                             efficacy_infection_bpsv = 0.35,                # vaccine efficacy against infection - BPSV
-#                                             efficacy_disease_bpsv = 0.75,                  # vaccine efficacy against disease - BPSV
-#                                             efficacy_infection_spec = 0.55,                # vaccine efficacy against infection - specific vaccine
-#                                             efficacy_disease_spec = 0.9,                   # vaccine efficacy against disease - specific vaccine
-#                                             dur_R = 365000000,                             # duration of infection-induced immunity
-#                                             dur_bpsv = 365000000,                          # duration of BPSV vaccine immunity
-#                                             dur_spec = 365000000,                          # duration of disease-specific vaccine immunity
-#                                             coverage = 0.8,                                # proportion of the population vaccinated
-#                                             vaccination_rate = 0.035,                      # vaccination rate per week as percentage of population
-#                                             seeding_cases = 5,                             # number of initial seeding cases
-#                                             min_age_group_index_priority = 13,             # index of the youngest age group given priority w.r.t vaccines (13 = 60+)
-#                                             min_age_group_index_non_priority = 4,          # index of the youngest age group that *receives* vaccines (4 = 15+)
-#                                             runtime = 1250)
-# 
-# raw_rel_start_scenarios2 <- expand_grid(raw_rel_start_scenarios, days_source_detection_is_ahead_arrival_secondary = seq(100, -100, -10))
-# raw_rel_start_scenarios2$detection_time_secondary <- 10 # need to change this to make it R0 specific
-# 
-# for (i in 1:nrow(raw_rel_start_scenarios2)) {
-#   if (raw_rel_start_scenarios2$days_source_detection_is_ahead_arrival_secondary[i] <= 0) { ## BPSV campaign and specific development starts after pathogen arrival
-#     raw_rel_start_scenarios2$detection_time[i] <- -raw_rel_start_scenarios2$days_source_detection_is_ahead_arrival_secondary[i]
-#     raw_rel_start_scenarios2$Rt[i] <- list(raw_rel_start_scenarios2$R0[i])
-#     raw_rel_start_scenarios2$tt_Rt[i] <- list(0)
-#   } else if (raw_rel_start_scenarios2$days_source_detection_is_ahead_arrival_secondary[i] > 0) { ## BPSV campaign and specific development starts ahead of pathogen arrival
-#     raw_rel_start_scenarios2$detection_time[i] <- 0
-#     raw_rel_start_scenarios2$Rt[i] <- list(c(1, raw_rel_start_scenarios2$R0[i]))
-#     raw_rel_start_scenarios2$tt_Rt[i] <- list(c(0, raw_rel_start_scenarios2$days_source_detection_is_ahead_arrival_secondary[i]))
-#   }
-# }
-# 
-# ## Creating overall output and index for output (important as it orders dataframe so that pairs of identical scenarios save for BPSV Y/N are next to each other)
-# vars_for_index <- c(variable_columns(raw_rel_start_scenarios2))
-# rel_start_scenarios <- raw_rel_start_scenarios2 %>%
-#   group_by(vaccine_scenario) %>%
-#   arrange_at(vars_for_index) %>%
-#   mutate(scenario_index = 1:n())
-# 
-# ## Running the model and summarising the output
-# cores <- parallel::detectCores() - 2
-# fresh_run <- TRUE
-# if (fresh_run) {
-#   plan(multisession, workers = cores) # multicore does nothing on windows as multicore isn't supported
-#   system.time({out <- future_pmap(rel_start_scenarios, run_sars_x, .progress = TRUE, .options = furrr_options(seed = 123))})
-#   model_outputs <- format_multirun_output(output_list = out, parallel = TRUE, cores = cores)
-#   saveRDS(model_outputs, "outputs/Figure4_PrimarySecondary_TimingComparison_NoNPIs_CalendarDays.rds")
-# } else {
-#   model_outputs <- readRDS("outputs/Figure4_PrimarySecondary_TimingComparison_NoNPIs_CalendarDays.rds")
-# }
-# 
-# ## Joining back in the detection metrics
-# rel_timing_df <- rel_start_scenarios %>%
-#   select(scenario_index, days_source_detection_is_ahead_arrival_secondary) %>%
-#   filter(vaccine_scenario == "specific_only") %>%
-#   ungroup() %>%
-#   select(-vaccine_scenario)
-# model_outputs2 <- model_outputs %>%
-#   left_join(rel_timing_df, by = c("scenario_index")) %>%
-#   filter(days_source_detection_is_ahead_arrival_secondary > -60)
-# 
-# ## Plotting secondary country days ahead advantage vs bpsv lives saved
-# population_size <- unique(model_outputs2$population_size)
-# temp_plot <- ggplot(model_outputs2, aes(x = -days_source_detection_is_ahead_arrival_secondary, y = bpsv_deaths_averted * 1000 / population_size, col = factor(R0))) +
-#   geom_line() +
-#   facet_wrap(specific_vaccine_start ~ ., 
-#              labeller = as_labeller(c(`100`='Specific Vaccine In 100 Days', 
-#                                       `200`='Specific Vaccine In 200 Days',
-#                                       `365`='Specific Vaccine In 365 Days'))) +
-#   theme_bw() +
-#   labs(x = "Days Pathogen Detection in Source Country is Ahead of Pathogen Arrival in Importing Country",
-#        y = "Deaths Averted By BPSV\n(Per 1,000 Population)") +
-#   theme(strip.placement = "outside",
-#         legend.position = "none",
-#         strip.background = element_rect(fill="white")) 
-
-
-
--- functions/runs_sars_x_secondary_country.R
+++ functions/runs_sars_x_secondary_country.R
@@ -3,6 +3,11 @@
 ### 7 = NPIs minimal mandate until BPSV campaign is done, followed by gradual release until spec vacc campaign done
 ### 8 = NPIs reducing Rt < 1 until BPSV campaign is done, followed by gradual release from minimal mandate until specific campaign is done
 ### 9 = No NPIs
+
+## -> TO DO: What remains somewhat unclear to me what would be the trigger in the secondary country for imposition of NPIs - probably detection in secondary country?
+##           Need to check this with Azra. 
+
+
 default_NPI_scenarios_secondary <- function(lockdown_Rt = 0.9,
                                             minimal_mandate_reduction = 0.25,
                                             NPI_scenarios = 1:9,
@@ -17,7 +22,7 @@ default_NPI_scenarios_secondary <- function(lockdown_Rt = 0.9,
   for (i in 1:nrow(scenarios)) {
     
     ## Where detection in origin country happens AFTER the arrival of the pathogen in the secondary country
-    ## BPSV campaign and specific development i.e. following pathogen detection in source country, starts AFTER pathogen arrival in secondary country
+    ## BPSV campaign and specific vaccine development (i.e. following pathogen detection in source country) starts AFTER pathogen arrival in secondary country
     ### Note - Try to avoid scenarios where days_source_detection_is_ahead_arrival_secondary is exactly 0 as it complicates things quite a lot.
     if (scenarios$days_source_detection_is_ahead_arrival_secondary[i] <= 0) {     
       scenarios$detection_time[i] <- -scenarios$days_source_detection_is_ahead_arrival_secondary[i]
@@ -37,7 +42,8 @@ default_NPI_scenarios_secondary <- function(lockdown_Rt = 0.9,
                          specific_vaccine_start = unique(scenarios$specific_vaccine_start),
                          vaccination_rate_bpsv = unique(scenarios$vaccination_rate_bpsv),
                          vaccination_rate_spec = unique(scenarios$vaccination_rate_spec),
-                         coverage = unique(scenarios$coverage),
+                         coverage_bpsv = unique(scenarios$coverage_bpsv),
+                         coverage_spec = unique(scenarios$coverage_spec),
                          min_age_group_index_priority = unique(scenarios$min_age_group_index_priority),
                          days_source_detection_is_ahead_arrival_secondary = unique(scenarios$days_source_detection_is_ahead_arrival_secondary),
                          detection_time_secondary = unique(scenarios$detection_time_secondary)) %>%
@@ -53,23 +59,22 @@ default_NPI_scenarios_secondary <- function(lockdown_Rt = 0.9,
     stop("Specific Vaccine start time happens before coverage with BPSV vaccine is finished. Change this.")
   }  
   
-  ## Timings when pathogen arrives in secondary country BEFORE detection in the source country - secondary country detection as the trigger for NPI imposition
+  ## Timings when pathogen arrives in secondary country BEFORE detection in the source country i.e. detection in origin country happens AFTER the arrival of the pathogen in the secondary country
+  ## - secondary country detection is the trigger for NPI imposition
   ## --> detection_time in source country is trigger for BPSV campaign, but detection_time_secondary is trigger for initial NPI imposition
   ##     We make the assumption that source country ALWAYS detects the pathogen first, and that because of enhanced surveillance following detection, the time until
   ##     the secondary country detects the virus is in calendar days (e.g. like 7 days) rather than dependent on number of hospitalisations.
   ## --> Note that detection_time_secondary here is relative to detection_time (which works because we have constraints that detection_time_secondary has to happen after detection_time)
-  ## -> TO DO: What remains somewhat unclear to me what would be the trigger in the secondary country for imposition of NPIs - probably detection in secondary country?
-  ##           Need to check this with Azra. 
   ## -> TO DO: Because of the way the triggers work (NPIs dependent on detection_time_secondary but BPSV campaign on detection_time), if detection_time_secondary > (bpsv_start + time_to_coverage_bpsv)
   ##           then campaign will be done by the time detection occurs, and hence NPI scenario 4 becomes identical to NPI scenario 9. I think we can get round this by having
   ##           detection_time_secondary to be just a small amount of time after detection_time. I've put a check in here to make sure of that, but probs we can do better.
   NPIs_arrival_before <- expand_grid(vacc_timings[vacc_timings$days_source_detection_is_ahead_arrival_secondary < 0], 
-                                    R0 = unique(scenarios$R0),
-                                    NPI_int = NPI_scenarios)
-  check <- (NPIs_arrival_before$detection_time + NPIs_arrival_before$detection_time_secondary) < (NPIs_arrival_before$detection_time + NPIs_arrival_before$bpsv_start + NPIs_arrival_before$time_to_coverage_bpsv)
-  if (sum(check) > 0) {
-    stop("For some scenarios where pathogen arrives in secondary country BEFORE detection, detection_time_secondary is occurring after the completion of the BPSV campaign - change this")
-  }
+                                     R0 = unique(scenarios$R0),
+                                     NPI_int = NPI_scenarios)
+  # check <- (NPIs_arrival_before$detection_time + NPIs_arrival_before$detection_time_secondary) < (NPIs_arrival_before$detection_time + NPIs_arrival_before$bpsv_start + NPIs_arrival_before$time_to_coverage_bpsv)
+  # if (sum(check) > 0) {
+  #   stop("For some scenarios where pathogen arrives in secondary country BEFORE detection, detection_time_secondary is occurring after the completion of the BPSV campaign - change this")
+  # }
   NPIs_arrival_before <- NPIs_arrival_before %>%
     rowwise() %>%
     mutate(temp = ((specific_vaccine_start + time_to_coverage_spec) - (bpsv_start + time_to_coverage_bpsv))) %>%
@@ -92,12 +97,12 @@ default_NPI_scenarios_secondary <- function(lockdown_Rt = 0.9,
   ### 9 = No NPIs
   ## Note gradient of the Rt increase in gradual lifting will vary sometimes here depending on comparative timing of different events
   NPIs_arrival_after <- expand_grid(vacc_timings[vacc_timings$days_source_detection_is_ahead_arrival_secondary > 0], 
-                                     R0 = unique(scenarios$R0),
-                                     NPI_int = NPI_scenarios)
-  check <- (NPIs_arrival_before$detection_time + NPIs_arrival_before$detection_time_secondary) > (NPIs_arrival_before$detection_time + NPIs_arrival_before$days_source_detection_is_ahead_arrival_secondary)
-  if (sum(check) > 0) {
-    stop("For some scenarios where pathogen arrives in secondary country BEFORE detection, detection_time_secondary is occurring after the completion of the BPSV campaign - change this")
-  }
+                                    R0 = unique(scenarios$R0),
+                                    NPI_int = NPI_scenarios)
+  # check <- (NPIs_arrival_before$detection_time + NPIs_arrival_before$detection_time_secondary) > (NPIs_arrival_before$detection_time + NPIs_arrival_before$days_source_detection_is_ahead_arrival_secondary)
+  # if (sum(check) > 0) {
+  #   stop("For some scenarios where pathogen arrives in secondary country BEFORE detection, detection_time_secondary is occurring after the completion of the BPSV campaign - change this")
+  # }
   NPIs_arrival_after <- NPIs_arrival_after %>%
     rowwise() %>%
     mutate(days_ago_bpsv_finish = detection_time_secondary - (bpsv_start + time_to_coverage_bpsv)) %>%                 ## remaining days of campaign bpsv after detection - if positive, bpsv campaign finishes before detection in secondary; if negative, bpsv campaign finishes after detection in secondary
