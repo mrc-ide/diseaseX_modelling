5cae97b87fc081b5660d0fd35301d47074f82550
CharlieWhittaker <cwhittaker1000@gmail.com>
2024-09-18 08:49:49 -0700

    filename clarification and updating

--- analysis/Figure1_Part1_BranchingProcess_RingVaccination_ContainmentExploration.R
+++ analysis/Figure1_Part1_BranchingProcess_RingVaccination_ContainmentExploration.R
@@ -6,15 +6,11 @@ source(here::here("functions/run_sars_x.R"))
 source(here::here("functions/branching_process.R"))
 source(here::here("functions/branching_process2.R"))
 
-## Currently missing:
-### Hospitalisations as a trigger for implementing the vaccine (i.e. detection and initiation not based on calendar time but case count)
-### Vaccine efficacy waning
-
 ### Fixed model parameters
 
 ### SARS-CoV-1-specific parameters (longer Tg, lower presymptomatic transmission, lower proportion asymptomatic infection)
-SC1_generation_time <- function(n) { rgamma(n, shape = 24, rate = 2) } ## Ask Azra for values ## NOTE that shape = 2, rate = 2 /24 would be equiv to SIR with 2 I compartments
-SC1_infection_to_onset <- function(n) { rgamma(n, shape = 0.1, rate = 1) } ## Ask Azra for values (negligible assumed currently)
+SC1_generation_time <- function(n) { rgamma(n, shape = 24, rate = 2) } 
+SC1_infection_to_onset <- function(n) { rgamma(n, shape = 0.1, rate = 1) } 
 SC1_prop_asymptomatic <- 0
 
 ### SARS-CoV-2-specific parameters (shorter Tg, higher presymptomatic transmission, higher proportion asymptomatic infection)
@@ -22,21 +18,21 @@ SC2_generation_time <- function(n) { rgamma(n, shape = 13.5, rate = 2) } # 6.75 
 SC2_infection_to_onset <- function(n) { rgamma(n, shape = 13.5/3, rate = 2) } # ~35% of transmission presymptomatic (per SARS-CoV-2, slightly lower than but roughly aligned with: https://bmjopen.bmj.com/content/11/6/e041240)
 SC2_prop_asymptomatic <- 0.15
 
-vaccine_start <- 21 # detection + logistical delay of 3 weeks to vaccination initiation (need to get implementation based on cases not calendar time in)
+vaccine_start <- 21 # detection + logistical delay of 3 weeks to vaccination initiation
 vaccine_coverage <- 0.8
 pop <- 10^10
 check_final_size <- 2500
 initial_immune <- 0
 seeding_cases <- 5
 
-vaccine_efficacy_infection <- 0.35 # 0.75
-vaccine_efficacy_transmission <- 0.35 # 0.50
+vaccine_efficacy_infection <- 0.35 
+vaccine_efficacy_transmission <- 0.35 
 vaccine_logistical_delay <- 2
 R0_scan <- c(0.75, 1, 1.25, 1.5, 1.75, 2, 2.25, 2.5)
 iterations <- 200
 
 ## R0 sensitivity analysis (Figure 1B)
-fresh_run_R0_sensitivity_analysis <- TRUE
+fresh_run_R0_sensitivity_analysis <- FALSE
 if (fresh_run_R0_sensitivity_analysis) {
   
   ### Setting up R0 scan and the storage for each of the different protection delays
@@ -249,9 +245,9 @@ if (fresh_run_R0_sensitivity_analysis) {
                          SC1_instant, SC2_instant) %>%
     pivot_longer(cols = starts_with("R0"), names_to = "R0", values_to = "Epidemic Size") %>%
     mutate(actualR0 = as.numeric(gsub("R0=", "", R0)))
-  saveRDS(object = overall_bp_df, file = "outputs/Figure1_branchingProcess_Containment/NEW_branchingProcess_R0_Scan.rds")
+  saveRDS(object = overall_bp_df, file = "outputs/Figure1_branchingProcess_Containment/Fig1BC_branchingProcess_ringVaccination_R0Scan.rds")
 } else {
-  overall_bp_df <- readRDS("outputs/Figure1_branchingProcess_Containment/NEW_branchingProcess_R0_Scan.rds")
+  overall_bp_df <- readRDS("outputs/Figure1_branchingProcess_Containment/Fig1BC_branchingProcess_ringVaccination_R0Scan.rds")
 }
 
 ## Plotting Figure 1B
@@ -284,11 +280,11 @@ containment_plot <- ggplot(containment_df2, aes(x = actualR0, y = 100 * proporti
                       guide = guide_legend(reverse = TRUE)) +
   labs(x = "R0", y = "% Outbreaks Contained") +
   theme(strip.background = element_rect(fill = "white"))
-ggsave(filename = "figures/Figure_1_BranchingProcess/NEW_Figure_1B_ContainmentPlot.pdf", plot = containment_plot, 
+ggsave(filename = "figures/Figure_1_BranchingProcess/Fig1BC_ContainmentPlot.pdf", plot = containment_plot, 
        width = 8, height = 3.1)
 
 ## Vaccination-Related Sensitivity Analyses Heatmaps
-fresh_run_vaccination_heatmaps <- TRUE
+fresh_run_vaccination_heatmaps <- FALSE
 if (fresh_run_vaccination_heatmaps) {
   
   ## R0 sensitivity analysis for all the runs
@@ -330,7 +326,7 @@ if (fresh_run_vaccination_heatmaps) {
     mutate(contained = ifelse(final_size < (0.9 * check_final_size), 1, 0)) %>%
     group_by(R0, TgRatio) %>%
     summarise(proportion_contained = sum(contained) / n())
-  saveRDS(object = storage_R0_TgRatio_df, file = "outputs/Figure1_branchingProcess_Containment/branchingProcess_R0_TgRatio_scan.rds")
+  saveRDS(object = storage_R0_TgRatio_df, file = "outputs/Figure1_branchingProcess_Containment/Fig1D_branchingProcess_ringVaccination_R0TgRatioScan.rds")
   
   ## Sensitivity Analysis - R0 vs Vaccine Efficacy
   Tg_ratio_fixed <- 2.5
@@ -368,7 +364,7 @@ if (fresh_run_vaccination_heatmaps) {
     mutate(contained = ifelse(final_size < (0.9 * check_final_size), 1, 0)) %>%
     group_by(R0, vaccine_efficacy) %>%
     summarise(proportion_contained = sum(contained) / n())
-  saveRDS(object = storage_R0_efficacy_df, file = "outputs/Figure1_branchingProcess_Containment/branchingProcess_R0_Efficacy_scan.rds")
+  saveRDS(object = storage_R0_efficacy_df, file = "outputs/Figure1_branchingProcess_Containment/Fig1E_branchingProcess_ringVaccination_R0EfficacyScan.rds")
   
   ## Sensitivity Analysis - R0 vs Presymptomatic Transmission Proportion
   Tg_ratio_fixed <- 2.5
@@ -406,12 +402,12 @@ if (fresh_run_vaccination_heatmaps) {
     mutate(contained = ifelse(final_size < (0.9 * check_final_size), 1, 0)) %>%
     group_by(R0, prop_preSymp) %>%
     summarise(proportion_contained = sum(contained) / n())
-  saveRDS(object = storage_R0_preSymp_df, file = "outputs/Figure1_branchingProcess_Containment/branchingProcess_R0_preSymp_scan.rds")
+  saveRDS(object = storage_R0_preSymp_df, file = "outputs/Figure1_branchingProcess_Containment/Fig1F_branchingProcess_ringVaccination_R0preSympScan.rds")
   
 } else {
-  storage_R0_TgRatio_df <- readRDS("outputs/Figure1_branchingProcess_Containment/branchingProcess_R0_TgRatio_scan.rds")
-  storage_R0_efficacy_df <- readRDS("outputs/Figure1_branchingProcess_Containment/branchingProcess_R0_Efficacy_scan.rds")
-  storage_R0_preSymp_df <- readRDS("outputs/Figure1_branchingProcess_Containment/branchingProcess_R0_preSymp_scan.rds")
+  storage_R0_TgRatio_df <- readRDS("outputs/Figure1_branchingProcess_Containment/Fig1D_branchingProcess_ringVaccination_R0TgRatioScan.rds")
+  storage_R0_efficacy_df <- readRDS("outputs/Figure1_branchingProcess_Containment/Fig1E_branchingProcess_ringVaccination_R0EfficacyScan.rds")
+  storage_R0_preSymp_df <- readRDS("outputs/Figure1_branchingProcess_Containment/Fig1F_branchingProcess_ringVaccination_R0preSympScan.rds")
 }
 
 ## Creating Vaccination-Related Heatmaps
@@ -430,7 +426,7 @@ R0_TgRatio_plot <- ggplot(storage_R0_TgRatio_df, aes(x = R0, y = TgRatio, fill =
         legend.position = "none",
         panel.border = element_rect(linetype = "solid", fill = NA, linewidth = 0.5)) +  # Add black border
   coord_cartesian(expand = FALSE)
-ggsave(file = "figures/Figure_1_BranchingProcess/Figure_1C_TgRatio_Sensitivity_Analysis.pdf", plot = R0_TgRatio_plot, width = 2.4, height = 2.4)
+ggsave(file = "figures/Figure_1_BranchingProcess/Fig1C_TgRatio_Sensitivity_Analysis.pdf", plot = R0_TgRatio_plot, width = 2.4, height = 2.4)
 
 ### Vaccine Efficacy
 R0_vaccine_efficacy_plot <- ggplot(storage_R0_efficacy_df, aes(x = R0, y = 100 * vaccine_efficacy, fill = proportion_contained)) +
@@ -446,7 +442,7 @@ R0_vaccine_efficacy_plot <- ggplot(storage_R0_efficacy_df, aes(x = R0, y = 100 *
         legend.position = "none",
         panel.border = element_rect(linetype = "solid", color = "black", fill = NA, linewidth = 0.5)) +  # Add black border
   coord_cartesian(expand = FALSE)
-ggsave(filename = "figures/Figure_1_BranchingProcess/Figure_1D_VaccineEfficacy_Sensitivity_Analysis.pdf", plot = R0_vaccine_efficacy_plot, width = 2.4, height = 2.4)
+ggsave(filename = "figures/Figure_1_BranchingProcess/Fig1D_VaccineEfficacy_Sensitivity_Analysis.pdf", plot = R0_vaccine_efficacy_plot, width = 2.4, height = 2.4)
 
 ### Proportion of Presymptomatic Transmission
 R0_preSymp_plot <- ggplot(storage_R0_preSymp_df, aes(x = R0, y = 100 * prop_preSymp, fill = proportion_contained)) +
@@ -462,68 +458,9 @@ R0_preSymp_plot <- ggplot(storage_R0_preSymp_df, aes(x = R0, y = 100 * prop_preS
         legend.position = "none",
         panel.border = element_rect(linetype = "solid", fill = NA, linewidth = 0.5)) +  # Add black border
   coord_cartesian(expand = FALSE)
-ggsave(filename = "figures/Figure_1_BranchingProcess/Figure_1E_preSymp_Sensitivity_Analysis.pdf", 
+ggsave(filename = "figures/Figure_1_BranchingProcess/Fig1E_preSymp_Sensitivity_Analysis.pdf", 
        plot = R0_preSymp_plot, width = 2.4, height = 2.4)
 
 ## Heatmaps legend
 legend <- R0_TgRatio_plot + theme(legend.position = "bottom")
-ggsave(file = "figures/Figure_1_BranchingProcess/Legend_vaccinationHeatmap.pdf", plot = legend, width = 2.4, height = 2.4)
-
-
-############################ COME BACK TO THIS #######################
-
-## Sensitivity Analysis - R0 vs Vaccine Efficacy MONOCLONAL
-R0_seq <- R0_scan[-c(1, 2)]
-vaccine_efficacy_seq <- seq(0.1, 1, 0.1)
-iterations <- 50
-storage_R0_efficacy_sensitivity <- array(data = NA, dim = c(length(R0_seq), length(vaccine_efficacy_seq), iterations))
-for (i in 1:length(R0_seq)) {
-  for (j in 1:length(vaccine_efficacy_seq)) {
-    for (k in 1:iterations) {
-      bp_out <- chain_sim_susc_ring_vacc2(offspring = "pois",
-                                          mn_offspring = R0_seq[i],
-                                          generation_time = generation_time,
-                                          t0 = 0, tf = Inf, pop = 10^7, 
-                                          check_final_size = check_final_size, initial_immune = 0,
-                                          seeding_cases = 5, prop_asymptomatic = 0.2,
-                                          infection_to_onset = infection_to_onset,
-                                          vaccine_start = vaccine_start, vaccine_coverage = vaccine_coverage,
-                                          vaccine_efficacy_infection = vaccine_efficacy_seq[j],
-                                          vaccine_efficacy_transmission = vaccine_efficacy_seq[j],
-                                          vaccine_logistical_delay = 2,
-                                          vaccine_protection_delay = 0)
-      storage_R0_efficacy_sensitivity[i, j, k] <- sum(!is.na(bp_out$time_infection))
-    }
-  }
-  print(i)
-}
-storage_R0_efficacy_df <- reshape2::melt(storage_R0_efficacy_sensitivity)
-storage_R0_efficacy_df$R0 <- rep(R0_seq, length(vaccine_efficacy_seq) * iterations)
-storage_R0_efficacy_df$vaccine_efficacy <- rep(vaccine_efficacy_seq, each = length(R0_seq), times = iterations)
-storage_R0_efficacy_df$iteration <- rep(1:iterations, each = length(R0_seq) * length(vaccine_efficacy_seq))
-storage_R0_efficacy_df <- storage_R0_efficacy_df %>%
-  select(-(Var1:Var3)) %>%
-  rename(final_size = value) %>%
-  mutate(contained = ifelse(final_size < (0.9 * check_final_size), 1, 0)) %>%
-  group_by(R0, vaccine_efficacy) %>%
-  summarise(proportion_contained = sum(contained) / n())
-saveRDS(object = storage_R0_efficacy_df, file = "outputs/branching_process_outputs/branchingProcess_R0_Efficacy_scan_Monoclonal.rds")
-
-storage_R0_efficacy_df <- readRDS("outputs/branching_process_outputs/branchingProcess_R0_Efficacy_scan_Monoclonal.rds")
-R0_vaccine_efficacy_plot <- ggplot(storage_R0_efficacy_df, aes(x = R0, y = vaccine_efficacy, fill = proportion_contained)) +
-  geom_tile(colour = "black") +
-  scale_fill_viridis_c(option = "rocket", limits = c(0, 1), begin = 0.25, end = 1, 
-                       name = "Proportion\nContained",
-                       direction = -1) +
-  scale_fill_distiller(palette = "PuRd", direction = 1,
-                       name = "Proportion\nContained") +
-  labs(x = "R0",
-       y = "Monoclonal Efficacy") +
-  theme(axis.text = element_text(angle = 0),
-        plot.title = element_text(hjust = 0.5, size = 20, face = "bold"),
-        legend.title = element_text(size = 12),
-        legend.text = element_text(size = 12),
-        legend.position = "none",
-        panel.border = element_rect(linetype = "solid", color = "black", fill = NA, linewidth = 0.5)) +  # Add black border
-  coord_cartesian(expand = FALSE)
-
+ggsave(file = "figures/Figure_1_BranchingProcess/Legend_ringVaccinationHeatmap.pdf", plot = legend, width = 2.4, height = 2.4)
