8d50a9f8315fdc0bcc68dd25fc6805bcdcaea80a
Charlie Whittaker <cwhittaker1000@gmail.com>
2025-03-12 11:59:28 +0000

    quarantine now in ring vaccination

--- functions/ring_vax_simulation.R
+++ functions/ring_vax_simulation.R
@@ -1,16 +1,16 @@
 offspring = c("pois") 
-mn_offspring = 5
+mn_offspring = 3
 disp_offspring = 1
 generation_time = function(n) {return(rep(10, n))}
-prop_asymptomatic = 0.5
+prop_asymptomatic = 0.1
 infection_to_onset = function(n) {return(rep(5, n))}
 vaccine_start = 0
-vaccine_coverage = 0.7
-vaccine_efficacy_infection = 0.5
+vaccine_coverage = 0.8
+vaccine_efficacy_infection = 0.75
 vaccine_efficacy_transmission = 0.2
 vaccine_logistical_delay = 1
 vaccine_protection_delay = 1
-prob_quarantine = 0.75
+prob_quarantine = 0
 onset_to_quarantine = function(n) {return (rep(1, n))}
 quarantine_efficacy = 0.5
 t0 = 0
@@ -20,6 +20,31 @@ check_final_size = 10000
 initial_immune = 0
 seeding_cases = 5
 seed = 200
+
+x <- ring_vax_bp_sim(offspring = offspring,
+                     mn_offspring = mn_offspring,
+                     disp_offspring = disp_offspring, 
+                     generation_time = generation_time,                   
+                     prop_asymptomatic = prop_asymptomatic,                 
+                     infection_to_onset = infection_to_onset,                
+                     vaccine_start = vaccine_start,                     
+                     vaccine_coverage = vaccine_coverage,                  
+                     vaccine_efficacy_infection = vaccine_efficacy_infection,        
+                     vaccine_efficacy_transmission = vaccine_efficacy_transmission,     
+                     vaccine_logistical_delay = vaccine_logistical_delay,          
+                     vaccine_protection_delay = vaccine_protection_delay,          
+                     prob_quarantine = prob_quarantine,                   
+                     onset_to_quarantine = onset_to_quarantine,               
+                     quarantine_efficacy = quarantine_efficacy,               
+                     t0 = 0,                            
+                     tf = Inf,                          
+                     population = population,                        
+                     check_final_size = check_final_size,                  
+                     initial_immune = 0,                
+                     seeding_cases = seeding_cases,                     
+                     seed = seed)
+
+head(x)
 
 ring_vax_bp_sim <- function(## Transmission Parameters
                             offspring = c("pois", "nbinom"),   # offspring distribution 
